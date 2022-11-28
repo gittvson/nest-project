663b77719c8bd6f61fdac3e5bf26f1dec46fcca8
Son <son.tran@alephon.com>
2022-11-28 17:22:59 +0700

    fix : create Join query

--- src/helper/generateSQL/joinWithSpecificTable.ts
+++ src/helper/generateSQL/joinWithSpecificTable.ts
@@ -1,42 +1,19 @@
-import { table } from 'console';
-
 export default function (listTable) {
-  let sql = getSelectStatement(listTable);
-  let index;
+  let sql = getSelectStatementV2(listTable);
+  let listJoinedTables = [];
+  let firstTime = true;
   for (let i = 0; i < listTable.length; i++) {
-    let joinKeys;
-    joinKeys = getSameKeys(
-      Object.values(listTable[i].object),
-      Object.values(listTable[i + 1].object),
-    );
-    sql +=
-      (i === 0
-        ? ` \nFROM "${listTable[i].tableName}" \nJOIN "${
-            listTable[i + 1].tableName
-          }"`
-        : ` \nJOIN "${listTable[i + 1].tableName}"`) +
-      getConditionsForJoin(
-        joinKeys,
-        listTable[i].tableName,
-        listTable[i + 1].tableName,
-      );
-    if (listTable[i + 1] && listTable[i + 1].isNotInSequence) break;
-    index = listTable[i].joinedTableName
-      ? getIndexOfTable(listTable[i].joinedTableName, listTable)
-      : i + 1;
-    joinKeys = getSameKeys(
-      Object.values(listTable[i].object),
-      Object.values(listTable[index].object),
-    );
-    sql +=
-      (i === 0
-        ? ` \nFROM "${listTable[i].tableName}" \nJOIN "${listTable[index].tableName}"`
-        : ` \nJOIN "${listTable[index].tableName}"`) +
-      getConditionsForJoin(
-        joinKeys,
-        listTable[i].tableName,
-        listTable[index].tableName,
-      );
+    listJoinedTables = listTable[i].joinedTables;
+    if (listJoinedTables && listJoinedTables.length > 0) {
+      for (let j = 0; j < listJoinedTables.length; j++) {
+        sql += getFullJoinCondition(
+          listTable[i],
+          listTable[getIndexOfTable(listJoinedTables[j], listTable)],
+          firstTime,
+        );
+        if (firstTime) firstTime = false;
+      }
+    }
   }
 
   return sql;
@@ -63,6 +40,34 @@ function getSelectStatement(listTable) {
   return sql.slice(0, -1);
 }
 
+function getSelectStatementV2(listTable) {
+  let sql = 'SELECT ';
+  let index = 0;
+  for (let i = 0; i < listTable.length; i++) {
+    if (listTable[i].main !== true) {
+      for (const key of Object.keys(listTable[i].object)) {
+        sql += getStringJsonForKey(
+          listTable[i].tableName,
+          listTable[i].object[key],
+          key,
+          index === 0
+            ? 'first'
+            : index === Object.keys(listTable[i].object).length - 1
+            ? 'last'
+            : 'normal',
+        );
+        index++;
+      }
+    } else {
+      for (const key of Object.keys(listTable[i].object)) {
+        sql += ` "${listTable[i].tableName}"."${listTable[i].object[key]}" as "${key}",`;
+      }
+    }
+    index = 0;
+  }
+  return sql.slice(0, -1);
+}
+
 function getConditionsForJoin(joinKeys, tableName1, tableName2) {
   let result = '';
   for (let i = 0; i < joinKeys.length; i++) {
@@ -75,8 +80,40 @@ function getConditionsForJoin(joinKeys, tableName1, tableName2) {
 
 function getIndexOfTable(tableName, listTable) {
   for (let i = 0; i < listTable.length; i++) {
-    if (listTable[i].tableName == table) {
+    if (listTable[i].tableName == tableName) {
       return i;
     }
   }
+}
+
+function getFullJoinCondition(table1, table2, first) {
+  let joinKeys = getSameKeys(
+    Object.values(table1.object),
+    Object.values(table2.object),
+  );
+  return (
+    (first
+      ? ` \nFROM "${table1.tableName}" \nJOIN "${table2.tableName}"`
+      : ` \nJOIN "${table2.tableName}"`) +
+    getConditionsForJoin(joinKeys, table1.tableName, table2.tableName)
+  );
+}
+
+function getStringJsonForKey(tableName, name, nameAlias, position) {
+  if (position === 'first') {
+    // if (nameAlias.toLowerCase().indexOf('id') !== -1)
+    //   return `'{"${nameAlias}":"'||"${tableName}"."${name}"||'",'||`;
+    // else
+    return `'{"${nameAlias}":"'||COALESCE("${tableName}"."${name}",'null')||'",'||`;
+  }
+  if (position === 'last') {
+    // if (nameAlias.toLowerCase().indexOf('id') !== -1)
+    //   return `'"${nameAlias}":"'||"${tableName}"."${name}"||'"}' as "${tableName}",`;
+    // else
+    return `'"${nameAlias}":"'||COALESCE("${tableName}"."${name}",'null')||'"}' as "${tableName}",`;
+  }
+  // if (nameAlias.toLowerCase().indexOf('id') !== -1)
+  //   return `'"${nameAlias}":"'||"${tableName}"."${name}"||'",'||`;
+  // else
+  return `'"${nameAlias}":"'||COALESCE("${tableName}"."${name}",'null')||'",'||`;
 }
